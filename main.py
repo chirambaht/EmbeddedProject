5984f7d22540871608e5180db66bb493c4849785
Humphrey Chiramba <chirambaht@gmail.com>
2019-10-07 15:20:21 +0200

    First Outline of code for Humphrey

--- main.py
+++ main.py
@@ -1,3 +1,46 @@
 #This is how the code will be divided up:
 # Humphrey: Fix Vout in schematic, I2C: RTC + Alarm, SPI : ADC and DAC
 # Tatenda: Buttons: Interrupts, Blynk linking
+
+import RPi.GPIO as GPIO
+import time
+
+def init_SPI():
+    print("SPI setup")
+
+def init_I2C():
+    print("I2C setup")
+
+def sound_alarm():
+    pass
+
+def read_from_ADC():
+    pass
+
+def write_to_dac():
+    pass
+
+def setup():
+    init_I2C()
+    init_SPI()
+
+def main():
+    print("write your logic here")
+
+
+# Only run the functions if
+if __name__ == "__main__":
+    # Make sure the GPIO is stopped correctly
+    try:
+        setup()
+        while True:
+            main()
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Exiting gracefully")
+        # Turn off your GPIOs here
+        GPIO.cleanup()
+    except Exception as e:
+        GPIO.cleanup()
+        print("Some other error occurred")
+        print(e.message)
